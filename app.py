c3121e96188bc26580f1686b0dfd08b0d1c53d9b
snapz12 <snapzofflicial0@gmail.com>
2026-09-23 15:30:40 +0530

    Updated gmail name

--- app.py
+++ app.py
@@ -57,9 +57,9 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
 
-app.config["MAIL_USERNAME"] = "slovzanofflicial0@gmail.com"
+app.config["MAIL_USERNAME"] = "slovzan21@gmail.com"
 app.config["MAIL_PASSWORD"] = "hmwczqojooyjrvte"
-app.config["MAIL_DEFAULT_SENDER"] = "slovzanofflicial0@gmail.com"
+app.config["MAIL_DEFAULT_SENDER"] = "slovzan21@gmail.com"
 
 mail = Mail(app)
 
